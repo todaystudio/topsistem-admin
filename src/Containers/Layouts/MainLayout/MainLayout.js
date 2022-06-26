1d4205e2cbe574273a98c7b398b4490f406d8929
todaystudio <58150891+todaystudio@users.noreply.github.com>
2022-06-26 20:26:37 +0300

    1.0.1

--- src/Containers/Layouts/MainLayout/MainLayout.js
+++ src/Containers/Layouts/MainLayout/MainLayout.js
@@ -1,71 +1,70 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu } from 'antd';
+import {Avatar, Layout, Menu, Space, Typography} from 'antd';
 import React from 'react';
-const { Header, Content, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-    const key = String(index + 1);
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
-        children: new Array(4).fill(null).map((_, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `option${subKey}`,
-            };
-        }),
-    };
-});
+import './MainLayout.css'
+import {Logo} from "../../../Components/Logo/Logo";
+import {menuData} from "../../../data/menu";
+import {Link} from "react-router-dom";
+import {QuestionCircleOutlined} from "@ant-design/icons";
+import {useNavigate} from "react-router-dom";
 
-export const MainLayout = () => (
-    <Layout>
-        <Header className="header">
-            <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
-        </Header>
+const {Header, Content, Sider} = Layout;
+const {Text} = Typography
+
+
+export const MainLayout = ({children}) => {
+    const navigate = useNavigate()
+
+    return (
         <Layout>
-            <Sider width={200} className="site-layout-background">
-                <Menu
-                    mode="inline"
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
+            <Header className="header mainLayout__header">
+                <div className="logo"><Logo/></div>
+                <div className='mainLayout__profile-wrap'>
+                    <Link to="/">
+                        <QuestionCircleOutlined style={{color: 'white', width: 13, marginRight: 15}}/>
+                    </Link>
+                    <Link to="/profile">
+                        <Space direction="horizontal">
+                            <Avatar src="https://joeschmoe.io/api/v1/random"/>
+                            <Text style={{color: '#fff'}}>Shopping Center</Text>
+                        </Space>
+                    </Link>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{
+                            height: '100%',
+                            borderRight: 0,
+                        }}
+                        items={menuData}
+                        onSelect={(item) => {
+                            navigate(item.item.props.path, {replace: true})
+                        }}
+                    />
+                </Sider>
+                <Layout
                     style={{
-                        height: '100%',
-                        borderRight: 0,
-                    }}
-                    items={items2}
-                />
-            </Sider>
-            <Layout
-                style={{
-                    padding: '0 24px 24px',
-                }}
-            >
-                <Breadcrumb
-                    style={{
-                        margin: '16px 0',
+                        padding: '24px 24px 24px',
                     }}
                 >
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        padding: 24,
-                        margin: 0,
-                        minHeight: 280,
-                    }}
-                >
-                    Content
-                </Content>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                            background: 'transparent'
+
+                        }}
+                    >
+                        {children}
+                    </Content>
+                </Layout>
             </Layout>
         </Layout>
-    </Layout>
-);
+    )
+};
